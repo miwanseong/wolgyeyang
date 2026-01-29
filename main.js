f104a5e3c7cdafbd1fd9da5a63bcb871eb9803f1
a01039244935 <a01039244935@gmail.com>
2026-01-29 05:11:28 +0000

    feat: Add Firebase SDK and initialize in main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js';
+import { firebaseConfig } from './firebase-config.js';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export auth and db for use in other modules
+export { auth, db };
+
 class VtuberCard extends HTMLElement {
     constructor() {
         super();
