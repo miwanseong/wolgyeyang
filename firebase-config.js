d9ff3904a6b3f1f8cfe77134b0a368a5cc7a011e
a01039244935 <a01039244935@gmail.com>
2026-02-03 03:06:15 +0000

    Update firebase-config.js with provided credentials

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB2-RUKaZ_sJB05lhFFdFOuix5hvAM1S3k",
+  authDomain: "wolgyeyang-project.firebaseapp.com",
+  projectId: "wolgyeyang-project",
+  storageBucket: "wolgyeyang-project.firebasestorage.app",
+  messagingSenderId: "380265463148",
+  appId: "1:380265463148:web:f4f4bcdb7f459d8da40749",
+  measurementId: "G-VBDB32KZ8H"
+};
